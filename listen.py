97be5b81c115cb7b0a3e1d4be9319eb4c4ee5051
samling <samlingx@gmail.com>
2018-08-21 00:03:39 -0700

    Listeners now take a test photo when triggered; response time variance is currently 1-4ms

--- listen.py
+++ listen.py
@@ -1,16 +1,24 @@
+from picamera import PiCamera
 import pigpio
 import RPi.GPIO as GPIO
 import time
 
+camera = PiCamera()
+camera.resolution = (640, 480)
 INPUT_PIN=11
 pi = pigpio.pi('localhost')
 
 def handler(pin):
     millis=int(round(time.time() * 1000))
+    camera.capture("%s.jpg" % millis)
     print("Change detected in pin", INPUT_PIN)
     print(millis)
 
 def main():
+    camera.start_preview()
+    print("Warming up camera...")
+    time.sleep(2)
+
     for g in range(0,32):
         print("gpio {} is {}".format(g, pi.read(g)))
 
